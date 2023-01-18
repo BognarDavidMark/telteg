9ae1937a40f0034866ec2931c410435673cf68e6
Bognár Dávid <bognardavidmark@gmail.com>
2023-01-18 22:32:55 +0100

    vege

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,13 @@
+/* 
+File: app.component.html
+Author: Bognár Dávid Márk
+Copyright: 2022, Bognár Dávid
+Group: Szoft-II-N
+Date: 2023-01-18
+Github: https://github.com/BognarDavidMark/
+Licenc: GNU GPL
+*/
+
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
+/* 
+File: app.component.html
+Author: Bognár Dávid Márk
+Copyright: 2022, Bognár Dávid
+Group: Szoft-II-N
+Date: 2023-01-18
+Github: https://github.com/BognarDavidMark/
+Licenc: GNU GPL
+*/
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
